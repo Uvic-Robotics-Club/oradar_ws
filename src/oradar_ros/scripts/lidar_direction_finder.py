762841abbe5f3e4755c525f89bc17046d5fa4559
MyJetson <stevenlandriault@gmail.com>
2023-07-19 21:46:35 -0700

    solved discontinuity at 0 issue which should help with direction picking in threshold_free_space

--- src/oradar_ros/scripts/lidar_direction_finder.py
+++ src/oradar_ros/scripts/lidar_direction_finder.py
@@ -11,17 +11,22 @@ from scipy.signal import savgol_filter
 # Set up plot
 fig, ax = plt.subplots()
 
-# Flag to enable or disable plotting
-PLOT_ENABLED_DISTANCE = True
-PLOT_ENABLED_SCORE = False
-PLOT_ENABLED_FREE_SPACE = False # Flag for plotting free space
 
-ROBOT_WIDTH = .4
-ROBOT_LENGTH = 0.4
+
+ROBOT_WIDTH = .05
+THRESHOLD = .2
 LIDAR_FOV = 360
 LIDAR_RESOLUTION = .8
 LIDAR_MAX_RANGE = 12  # meters
 LiDAR_CHANGE_RANGE = 0.5 # meters
+
+# Flag to enable or disable plotting
+PLOT_ENABLED_DISTANCE = False
+PLOT_ENABLED_SCORE = False
+PLOT_ENABLED_FREE_SPACE = True #True # Flag for plotting free space
+PLOT_ENABLED_MIDPOINT = False
+PLOT_ENABLED_DATA_RANGES = False
+
 
 # Position of the robot.
 robot_position = [0, 0]
@@ -91,33 +96,54 @@ def calculate_distance_changes(distances):
             changes.append(0)
     return changes
 '''
+
 '''
-def identify_free_space(distances, threshold):
+def threshold_free_space(distances, threshold):
     # Initialize free space list
     free_space = [0]*len(distances)
-    for i in range(1, len(distances)):
+    for i in range(0, len(distances)-1):
         # Change from blocked to free space
         if distances[i] > threshold:
-            free_space[i] = distances[i]
+            free_space[i] = threshold;
+            # free_space[i] = distances[i] plots distance
         # Change from free to blocked space
         elif distances[i] <= threshold:
             free_space[i] = 0
-
+    print(distances[1])
     return free_space
 '''
+#threshold_free_space starts at index n/2 and ends at index n/2 -1 this way directly in front of the rover does not possess a discontinuity
+def threshold_free_space(distances, threshold):
+    # Initialize free space list
+    free_space = [0]*len(distances)
+    
+    n = len(distances)
+    start = n // 2
+    
+    for i in range(n):
+        # Change from blocked to free space
+        index = (start + i) % n
+        if distances[index] > threshold:
+            free_space[index] = threshold
+        # Change from free to blocked space
+        elif distances[index] <= threshold:
+            free_space[index] = 0
+    
+    return free_space
 
 def identify_free_space(distances, threshold):
     # Initialize free space list
     free_space = [0]*len(distances)
-    for i in range(1, len(distances)):
+    for i in range(0, len(distances)-1):
         # Change from blocked to free space
         if distances[i] > threshold:
-            free_space[i] = distances[i]
+            free_space[i] = threshold
         # Change from free to blocked space
         elif distances[i] <= threshold:
             free_space[i] = 0
 
     # Identifying free spaces that are bounded by zeros on both sides
+    
     free_spaces = []
     start = None
     for i in range(1, len(free_space)-1):
@@ -128,24 +154,33 @@ def identify_free_space(distances, threshold):
         elif free_space[i] != 0 and free_space[i+1] == 0:
             if start is not None:
                 free_spaces.append((start, i))
+                
                 start = None
                 
     # Calculate the physical distances for each free space
     free_space_data = []
     for start, end in free_spaces:
+        #print("Start of Group " + str(start) + " End of Group " + str(end))
         a = free_space[start]
         b = free_space[end]
-        theta = abs(end - start)  # Assuming end and start are in degrees. Convert to radians if not.
+        #print("a is " +str(a) + "threshold" + str(threshold) + "b is " +str(b))
+        startrad = start*2*np.pi/len(free_space)
+        endrad = end*2*np.pi/len(free_space)
+        
+        
+        theta = abs(endrad - startrad)  # Assuming end and start are in degrees. Convert to radians if not.
         
         # Distance formula
-        distance = math.sqrt(a**2 + b**2 - 2*a*b*math.cos(math.radians(theta)))  # Use math.radians if theta is not in radians
+        distance = math.sqrt(a**2 + b**2 - 2*a*b*math.cos(theta))  # Use math.radians if theta is not in radians
+        #print("Distance is " + str(distance))
         free_space_data.append({"distance": distance, "angle_indices": (start, end)})
-    '''    
+        
     # Print the data
-    for fs in free_space_data:
-        print("Distance:", fs["distance"], "Angle indices:", fs["angle_indices"])
-    '''
+    #for fs in free_space_data:
+        #print("Distance:", fs["distance"], "Angle indices:", fs["angle_indices"])
+    
     return free_space_data
+
 
 def get_possible_directions(distances, threshold, rover_width):
     free_spaces = identify_free_space(distances, threshold)
@@ -155,8 +190,9 @@ def get_possible_directions(distances, threshold, rover_width):
         distance = fs['distance']
         if distance >= rover_width:
             angle_indices = fs['angle_indices']
-            midpoint = (angle_indices[0] + angle_indices[1]) / 2
+            midpoint = ((angle_indices[0] + angle_indices[1])%len(distances)) / 2
             possible_directions.append(midpoint)
+            #print(midpoint)
 
     return possible_directions
 
@@ -168,19 +204,19 @@ def plot_data(data):
     plt.draw()
     plt.pause(0.001)
     
-def plot_polar(data):
-    global ax
-    plt.cla() 
-    ax = plt.subplot(1, 1, 1, polar=True)  # Create a new subplot in polar format
-    theta = np.linspace(0, 2*np.pi, len(data))  # Create an array of angles
-    ax.plot(theta, data)  # Plot the distances at each angle
+'''
+def plot_free_space(free_space):
+    plt.cla()
+    ax = plt.subplot(1, 1, 1, polar=True)  
+    theta = np.linspace(0, 2*np.pi, len(free_space))  # Create an array of equally spaced angle values between 0 and 2*pi
+    ax.plot(theta, free_space)  # Plot the free_space data at each angle
     ax.set_theta_zero_location("N")  # Set 0 degrees to the top of the plot
     ax.set_theta_direction(-1)  # Make angles increase in a clockwise direction
-    ax.set_ylim([0, 0.5])  # Limit the range of distances to plot
     plt.draw()
     plt.pause(0.001)
+'''
 
-
+'''
 def plot_smoothed_distances(smoothed_distances):
     global ax
     plt.cla()
@@ -192,17 +228,30 @@ def plot_smoothed_distances(smoothed_distances):
     plt.pause(0.001)
 
 '''
-def plot_free_space(free_space):
+def plot_data_ranges(data_ranges):
     plt.cla()
-    ax = plt.subplot(1, 1, 1, polar=True)  # Create a new subplot in polar format
-    theta = np.linspace(0, 2*np.pi, len(free_space))  # Convert FOV angles to radians
-    ax.plot(theta, free_space)  # Plot the free_space at each angle
+    ax = plt.subplot(1, 1, 1, polar=True)  
+    theta = np.linspace(0, 2*np.pi, len(data_ranges))  # Create an array of equally spaced angle values between 0 and 2*pi
+    ax.plot(theta, data_ranges)  # Plot the smoothed_distances at each angle
+    ax.set_theta_zero_location("N")  # Set 0 degrees to the top of the plot
+    ax.set_theta_direction(-1)  # Make angles increase in a clockwise direction
+    plt.title("Data Ranges")
+    plt.draw()
+    plt.pause(0.001)
+
+
+
+def plot_free_space_polar(free_space):
+    plt.cla()
+    ax = plt.subplot(1, 1, 1, polar=True)  
+    theta = np.linspace(0, 2*np.pi, len(free_space))  # Create an array of equally spaced angle values between 0 and 2*pi
+    ax.plot(theta, free_space)  # Plot the free_space data at each angle
     ax.set_theta_zero_location("N")  # Set 0 degrees to the top of the plot
     ax.set_theta_direction(-1)  # Make angles increase in a clockwise direction
     plt.draw()
     plt.pause(0.001)
-'''
 
+'''
 def plot_free_space(free_space_data):
     plt.cla()
     ax = plt.subplot(1, 1, 1, polar=True)  # Create a new subplot in polar format
@@ -215,8 +264,9 @@ def plot_free_space(free_space_data):
     ax.set_theta_direction(-1)  # Make angles increase in a clockwise direction
     plt.draw()
     plt.pause(0.001)
-    
-def plot_polar(midpoints):
+'''
+  
+def plot_polar_midpoints(midpoints):
     # Convert angles to radians and normalize
     midpoints_radians = [np.radians(midpoint) for midpoint in midpoints]
     data = [1]*len(midpoints_radians)  # All angles have same priority
@@ -241,22 +291,33 @@ def plot_scores(scores, best_direction):
     plt.draw()
     plt.pause(0.001)
 
+
 def callback(data):
     global PLOT_ENABLED_DISTANCE, PLOT_ENABLED_SCORE, PLOT_ENABLED_FREE_SPACE, robot_position, goal_position
     current_time = rospy.get_time()
     finite_ranges = remove_nan_and_inf(data.ranges)
-    smoothed_distances = savgol_filter(finite_ranges, 5, 3)  
-    free_space = identify_free_space(smoothed_distances, ROBOT_LENGTH)
-    possible_directions = get_possible_directions(smoothed_distances, ROBOT_LENGTH, ROBOT_WIDTH)    
+  #  smoothed_distances = savgol_filter(finite_ranges, 5, 3)  
+    free_space = threshold_free_space(data.ranges, THRESHOLD)
+    possible_directions = get_possible_directions(data.ranges, THRESHOLD, ROBOT_WIDTH)    
     
-    if PLOT_ENABLED_DISTANCE and current_time - callback.last_update_time >= 1.0:
-        plot_polar(possible_directions)
+    if PLOT_ENABLED_DISTANCE and current_time - callback.last_update_time >= 1.0:     
+        #plot_free_space_polar(finite_ranges)
         callback.last_update_time = current_time
-
+     
+    if PLOT_ENABLED_DATA_RANGES and current_time - callback.last_update_time >= 1.0:
+    	#plot_data_ranges(finite_ranges) # subbed in 'data.ranges' for 'smoothed_distances'
+    	callback.last_update_time = current_time
+    	
     if PLOT_ENABLED_FREE_SPACE and current_time - callback.last_update_time >= 1.0:
-        free_space = identify_free_space(smoothed_distances, ROBOT_LENGTH)
-        plot_free_space(free_space)
+        #free_space = identify_free_space(finite_ranges, THRESHOLD)
+        plot_free_space_polar(free_space)
         callback.last_update_time = current_time
+    
+    
+    if PLOT_ENABLED_MIDPOINT and current_time - callback.last_update_time >= 1.0:
+    	#plot_polar_midpoints(possible_directions)
+    	callback.last_update_time = current_time
+    	
     
     goal_direction_dx = goal_position[0] - robot_position[0]
     goal_direction_dy = goal_position[1] - robot_position[1]
